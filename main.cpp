dd6021aee4a678b1864ccf9e08d0613344b10eeb
AxelBlusseau <36446232+AxelBlusseau@users.noreply.github.com>
2018-04-10 21:41:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+
+
     /******************************************
 
                     CLASSE CARTE
@@ -13,35 +15,41 @@ using namespace std;
 
 class Carte
 {
-private:
-    int valeur;
-    int couleur;
-    string couleurs;
-public:
-    Carte(int v,int c) //Constructeur
-    {
-        valeur=v;
-        couleur=c;
-    }
+    private:
+        int valeur;
+        int couleur;
+        string couleurs;
 
-    int getValeur() { return valeur; }
-
-    void afficher() {
-        if(couleur==1){
-            couleurs="Coeur";
-        }
-        if(couleur==2){
-            couleurs="Carreau";
-        }
-        if(couleur==3){
-            couleurs="Pique";
-        }
-        if(couleur==4){
-            couleurs="Trefle";
+    public:
+        Carte(int v,int c) //Constructeur
+        {
+            valeur=v;
+            couleur=c;
         }
 
-        cout << valeur << " " << couleurs << endl;
-    }
+        int getValeur() { return valeur; } //Accesseur
+
+        void afficher()
+        {
+            if(couleur==1)
+            {
+                couleurs="Coeur";
+            }
+            if(couleur==2)
+            {
+                couleurs="Carreau";
+            }
+            if(couleur==3)
+            {
+                couleurs="Pique";
+            }
+            if(couleur==4)
+            {
+                couleurs="Trefle";
+            }
+
+            cout << valeur << " " << couleurs << endl;
+        }
 };
 
     /******************************************
@@ -52,12 +60,13 @@ public:
 
 class PaquetDeCarte
 {
-    vector<Carte*> distribution;
+        vector<Carte*> distribution; //Constructeur
 
-public:
+    public:
 
 
-    Carte* retirerCarte(){
+    Carte* retirerCarte()
+    {
         Carte* t = distribution[0];
         distribution.erase(distribution.begin());
         return t;
@@ -65,7 +74,8 @@ public:
 
     //JE VEUX SELECTIONNER UNE CARTE DE MON PAQUET DE CARTES
 
-    Carte* getCarte(int i) {
+    Carte* getCarte(int i)
+    {
         return distribution[i];
     }
 
@@ -87,12 +97,20 @@ public:
 
     void supprimer3Cartes()
     {
-        distribution.erase(distribution.begin() + 0, distribution.begin() + 3);
+        distribution.erase(distribution.begin(), distribution.begin() + 3);
+    }
+
+    //JE VEUX SUPPRIMER LES 3 PREMIERES CARTES DE MON PAQUET
+
+    void supprimer5Cartes()
+    {
+        distribution.erase(distribution.begin(), distribution.begin() + 5);
     }
 
     //JE VEUX SAVOIR LA TAILLE DE MON PAQUET DE CARTES
 
-    int size(){
+    int size()
+    {
     return distribution.size();
     }
 
@@ -117,26 +135,70 @@ public:
 
     //J'AFFICHE MON PAQUET DE CARTE
 
-    void afficherPdc (){
+    void afficherPdc ()
+    {
         int oui;
-    for(oui = 0; oui < distribution.size(); oui++){
+        for(oui = 0; oui < distribution.size(); oui++)
+        {
         distribution[oui]->afficher();
-        //cout << distribution[oui].getValeur() << endl;
         }
     }
+
 };
+
+
+    /******************************************
+
+                    CLASSE JOUEUR
+
+     ******************************************/
+
+class Joueur
+{
+    private:
+        string nom;
+
+    public:
+        Joueur(string n)
+        {
+            nom = n;
+        }
+
+        string getNom()
+        {
+        return nom;
+        }
+
+};
+
+    /******************************************
+
+                    INT MAIN
+
+     ******************************************/
 
 int main()
 {
     srand (time(NULL));
 
-    //Création d'un paquet de carte
+    string nomJ1;
+    string nomJ2;
+
+
+    cout << "[ BATAILLE ]" << endl;
+    cout << "Veuillez préciser le nom du joueur 1 : ";
+    cin >> nomJ1;
+
+    cout << "Veuillez préciser le nom du joueur 2 : ";
+    cin >> nomJ2;
+
+    //Création du paquet de cartes
     PaquetDeCarte pdc;
 
     int i;
     int a;
 
-for(i=1; i<14; i++)
+    for(i=1; i<14; i++)
     {
         for(a=1; a<5; a++)
         {
@@ -147,46 +209,55 @@ for(i=1; i<14; i++)
 
 
     pdc.melangerCarte();
-
     pdc.afficherPdc();
 
     cout << endl;
     cout << endl;
-     PaquetDeCarte PdcJ1;
-     PaquetDeCarte PdcJ2;
 
-    for (int d=0;d<52;d++){
-    Carte* toto;
-    toto=pdc.retirerCarte();
+    PaquetDeCarte PdcJ1;
+    PaquetDeCarte PdcJ2;
 
-    if(d%2==1){
+    for (int d=0;d<52;d++)
+    {
+        Carte* toto;
+        toto=pdc.retirerCarte();
 
-    PdcJ1.ajouterUneCarte(toto);
-
-    } else {
-    PdcJ2.ajouterUneCarte(toto);
-
+        if(d%2==1)
+        {
+            PdcJ1.ajouterUneCarte(toto);
+        }else{
+        PdcJ2.ajouterUneCarte(toto);
+        }
     }
-    }
- PdcJ1.afficherPdc();
-cout <<endl<<endl;
- PdcJ2.afficherPdc();
 
- int o = 0;
+    Joueur Joueur1(nomJ1);
+    Joueur Joueur2(nomJ2);
 
- while (PdcJ1.size() > 0 && PdcJ2.size() > 0){
+   // PdcJ1.afficherPdc();
 
-        cout << "[" <<  o << "] : " << PdcJ1.getCarte(0)->getValeur() << " " << PdcJ2.getCarte(0)->getValeur() << endl;
+    cout <<endl<<endl;
+
+   // PdcJ2.afficherPdc();
+
+    int o = 0;
+
+    while (PdcJ1.size() > 0 && PdcJ2.size() > 0)
+    {
+        cout << "Cartes restantes dans le paquet de : " << Joueur1.getNom() << " " << PdcJ1.size() << endl;
+        cout << "Cartes restantes dans le paquet de : " << Joueur2.getNom() << " " << PdcJ2.size() << endl;
+
+        cout << "Tour [" <<  o << "] : " << PdcJ1.getCarte(0)->getValeur() << " contre " << PdcJ2.getCarte(0)->getValeur() << endl;
         o++;
 
-        if ( PdcJ1.getCarte(0)->getValeur() > PdcJ2.getCarte(0)->getValeur() ){
+        if ( PdcJ1.getCarte(0)->getValeur() > PdcJ2.getCarte(0)->getValeur() )
+        {
             PdcJ1.ajouterUneCarte(PdcJ2.getCarte(0));
             PdcJ1.ajouterUneCarte(PdcJ1.getCarte(0));
 
             PdcJ1.supprimerCarte();
             PdcJ2.supprimerCarte();
 
-            cout << "Le Joueur 1 a gagne ce tour" << endl;
+            cout << Joueur1.getNom() << " a gagne ce tour" << endl;
 
         } else if( PdcJ1.getCarte(0)->getValeur() < PdcJ2.getCarte(0)->getValeur() ){
             PdcJ2.ajouterUneCarte(PdcJ1.getCarte(0));
@@ -195,13 +266,14 @@ cout <<endl<<endl;
             PdcJ1.supprimerCarte();
             PdcJ2.supprimerCarte();
 
-            cout << "Le Joueur 2 a gagne ce tour" << endl;
+            cout << Joueur2.getNom() << " a gagne ce tour" << endl;
         }else{
-            cout << "Egalite !" << endl;
+        cout << "Egalite !" << endl;
 
-            cout << PdcJ1.getCarte(2)->getValeur() << " " << PdcJ2.getCarte(2)->getValeur() << endl;
+        cout << PdcJ1.getCarte(2)->getValeur() << " contre " << PdcJ2.getCarte(2)->getValeur() << endl;
 
-                if ( PdcJ1.getCarte(2)->getValeur() > PdcJ2.getCarte(2)->getValeur() ){
+                if ( PdcJ1.getCarte(2)->getValeur() > PdcJ2.getCarte(2)->getValeur() )
+                    {
                     PdcJ1.ajouterUneCarte(PdcJ2.getCarte(0));
                     PdcJ1.ajouterUneCarte(PdcJ1.getCarte(0));
                     PdcJ1.ajouterUneCarte(PdcJ2.getCarte(1));
@@ -212,36 +284,74 @@ cout <<endl<<endl;
                     PdcJ1.supprimer3Cartes();
                     PdcJ2.supprimer3Cartes();
 
-                    cout << "Le Joueur 1 a gagne ce tour" << endl;
+            cout << Joueur1.getNom() << " a gagne ce tour" << endl;
 
-                } else if( PdcJ1.getCarte(2)->getValeur() < PdcJ2.getCarte(2)->getValeur() ){
-                    PdcJ2.ajouterUneCarte(PdcJ1.getCarte(0));
-                    PdcJ2.ajouterUneCarte(PdcJ2.getCarte(0));
-                    PdcJ2.ajouterUneCarte(PdcJ1.getCarte(1));
-                    PdcJ2.ajouterUneCarte(PdcJ2.getCarte(1));
-                    PdcJ2.ajouterUneCarte(PdcJ1.getCarte(2));
-                    PdcJ2.ajouterUneCarte(PdcJ2.getCarte(2));
+                    } else if( PdcJ1.getCarte(2)->getValeur() < PdcJ2.getCarte(2)->getValeur() ){
+                        PdcJ2.ajouterUneCarte(PdcJ1.getCarte(0));
+                        PdcJ2.ajouterUneCarte(PdcJ2.getCarte(0));
+                        PdcJ2.ajouterUneCarte(PdcJ1.getCarte(1));
+                        PdcJ2.ajouterUneCarte(PdcJ2.getCarte(1));
+                        PdcJ2.ajouterUneCarte(PdcJ1.getCarte(2));
+                        PdcJ2.ajouterUneCarte(PdcJ2.getCarte(2));
 
-                    PdcJ1.supprimer3Cartes();
-                    PdcJ2.supprimer3Cartes();
+                        PdcJ1.supprimer3Cartes();
+                        PdcJ2.supprimer3Cartes();
 
-                    cout << "Le Joueur 2 a gagne ce tour" << endl;
+            cout << Joueur2.getNom() << " a gagne ce tour" << endl;
+                    }else{
+                        cout << "Encore egalite !" << endl;
+                        cout << PdcJ1.getCarte(4)->getValeur() << " contre " << PdcJ2.getCarte(4)->getValeur() << endl;
+
+                         if ( PdcJ1.getCarte(4)->getValeur() > PdcJ2.getCarte(4)->getValeur() )
+                         {
+                            PdcJ1.ajouterUneCarte(PdcJ2.getCarte(0));
+                            PdcJ1.ajouterUneCarte(PdcJ1.getCarte(0));
+                            PdcJ1.ajouterUneCarte(PdcJ2.getCarte(1));
+                            PdcJ1.ajouterUneCarte(PdcJ1.getCarte(1));
+                            PdcJ1.ajouterUneCarte(PdcJ2.getCarte(2));
+                            PdcJ1.ajouterUneCarte(PdcJ1.getCarte(2));
+                            PdcJ1.ajouterUneCarte(PdcJ2.getCarte(3));
+                            PdcJ1.ajouterUneCarte(PdcJ1.getCarte(3));
+                            PdcJ1.ajouterUneCarte(PdcJ2.getCarte(4));
+                            PdcJ1.ajouterUneCarte(PdcJ1.getCarte(4));
+
+                            PdcJ1.supprimer5Cartes();
+                            PdcJ2.supprimer5Cartes();
+
+            cout << Joueur1.getNom() << " a gagne ce tour" << endl;
+
+                        } else if( PdcJ1.getCarte(4)->getValeur() < PdcJ2.getCarte(4)->getValeur() ){
+                            PdcJ2.ajouterUneCarte(PdcJ1.getCarte(0));
+                            PdcJ2.ajouterUneCarte(PdcJ2.getCarte(0));
+                            PdcJ2.ajouterUneCarte(PdcJ1.getCarte(1));
+                            PdcJ2.ajouterUneCarte(PdcJ2.getCarte(1));
+                            PdcJ2.ajouterUneCarte(PdcJ1.getCarte(2));
+                            PdcJ2.ajouterUneCarte(PdcJ2.getCarte(2));
+                            PdcJ2.ajouterUneCarte(PdcJ1.getCarte(3));
+                            PdcJ2.ajouterUneCarte(PdcJ2.getCarte(3));
+                            PdcJ2.ajouterUneCarte(PdcJ1.getCarte(4));
+                            PdcJ2.ajouterUneCarte(PdcJ2.getCarte(4));
+
+                            PdcJ1.supprimer5Cartes();
+                            PdcJ2.supprimer5Cartes();
+
+            cout << Joueur2.getNom() << " a gagne ce tour" << endl;
+                        }
+                    }
         }
+
+    //DU COUPS, S'IL Y A TROIS FOIS DE SUITE UNE EGALITE YA UN PROBLEME, ON AURAIT PU REFAIRE UN AUTRE ELSE POUR PARER CETTE SITUATION
+
     }
-
-    //DU COUPS, S'IL Y A DEUX FOIS DE SUITE UNE EGALITE YA UN PROBLEME.
-
- }
-        if (PdcJ1.size() == 0){
-        cout << "JOUEUR [1] GAGNE"<< endl;
+        if (PdcJ1.size() <= 0)
+        {
+        cout << Joueur2.getNom() << " GAGNE" << endl;
         }
-        if (PdcJ2.size() == 0){
-        cout << "JOUEUR [2] GAGNE"<< endl;
+        if (PdcJ2.size() <= 0)
+        {
+        cout << Joueur1.getNom() << " GAGNE" << endl;
         }
 }
-
-
-
 
 
 
